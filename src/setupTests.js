8461b2246fc5e8135f42429c977701cb4f68333e
David Nguyen <davidnguyen21@gmail.com>
2020-03-20 04:57:36 -0700

    setup eslint and tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,25 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+const mockGeolocation = {
+  getCurrentPosition: jest.fn(cb =>
+    cb({
+      coords: {
+        latitude: "lat",
+        longitude: "lng"
+      }
+    })
+  )
+};
+
+global.navigator.geolocation = mockGeolocation;
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
